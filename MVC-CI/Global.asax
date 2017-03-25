cc2d521d29520d9681e6513aae03cd116f4eead0
Biren Shah <biren@msn.com>
2017-03-25 12:17:29 -0400

    Add project files.

---
+++ MVC-CI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVC_CI.MvcApplication" Language="C#" %>
---
+++ MVC-CI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_CI.Startup))]
+namespace MVC_CI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
